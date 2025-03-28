cd570b50fcab1787304527d5fdb6e49e024053e5
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-03-28 13:11:29 -0300

    Repaso puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main () {
-    printf("Hola Mundo");
+    printf("Hola Mundo \n");
 
+    int numero = 10, *p_numero;
+
+    p_numero = &numero;
+
+    printf("El contenido del puntero: %d\n", *p_numero);
+    printf("La direccion de memoria almacenada por el puntero: %p\n", p_numero);
+    printf("La direccion de la memoria de la variable: %p\n", &numero);
+    printf("La direccion de memoria del puntero: %p\n", &p_numero);
+    printf("El tamanio de memoria de la variable 'num' es: %zu bytes\n", sizeof(numero));
+    printf("El tamanio de memoria del puntero es: %zu bytes\n", sizeof(p_numero)); 
+    
     return 0;
 }
